6d8d14a8bc8a54147d8963f9f3affaf4e39e22ab
mishrasv <77544653+mishrasv@users.noreply.github.com>
2021-02-07 10:52:20 -0500

    Models

---
+++ RecruitCatmishrasv/RecruitCatmishrasv/Models/Company.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecruitCatmishrasv.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PositionName { get; set; }
+        public decimal MinimumSalary { get; set; }
+        public decimal MaximumSalary { get; set; }
+        public DateTime? StartDate { get; set; }
+        public string Location { get; set; }
+        public bool MNC { get; set; }
+        public List<Candidate> Candidates { get; set; }
+
+    }
+}
---
+++ RecruitCatmishrasv/RecruitCatmishrasv/Models/JobTitle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecruitCatmishrasv.Models
+{
+    public class JobTitle
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal MinimumSalary { get; set; }
+        public decimal MaximumSalary { get; set; }
+        public decimal Bonus { get; set; }
+        public int ContractYear { get; set; }
+        public List<Candidate> Candidates { get; set; }
+
+
+    }
+}
